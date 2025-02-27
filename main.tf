98c79f12e0bf677d5dff700c1dd7dfd53d70e472
Mehmood Ur Rehman <115158928+MehmoodDevOPsEngineer@users.noreply.github.com>
2025-02-27 10:37:06 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+
 #Providing the azure account on which our resources will be created
 provider "azurerm" {
   features {}
